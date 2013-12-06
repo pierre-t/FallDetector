5f59308a698110887d44d3652ce966d94253a9a0
Diego Sogari <diego.sogari@gmail.com>
2013-12-06 04:58:40 -0200

    implementaçao da interface
    + comunicaçao entre activity e serviço
    + botao para iniciar/parar o serviço
    + botao para editar configuraçoes
    + painel com dados do sensor
    + checkbox liga/desliga o painel
    + atualizaçao do arquivo de strings

--- src/com/sisemb/falldetector/FallDetector.java
+++ src/com/sisemb/falldetector/FallDetector.java
@@ -6,17 +6,35 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Binder;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class FallDetector extends Service implements SensorEventListener {
+
+    private final IBinder _localBinder = new LocalBinder();
+    public static boolean _bServiceStarted = false;
+    private float _fSensorX;
+    private float _fSensorY;
+    private float _fSensorZ;
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        FallDetector getService() {
+            // Return this instance of FallDetector so clients can call public methods
+            return FallDetector.this;
+        }
+    }
 
 	@Override
 	public void onCreate() {
 		_sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		_acceleratorSensor =
 				_sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		
+
 		if (_acceleratorSensor == null) {
 			stopSelf();
 		}
@@ -24,41 +42,55 @@ public class FallDetector extends Service implements SensorEventListener {
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Toast.makeText(this, "Starting fall detector", Toast.LENGTH_SHORT)
+		Toast.makeText(this, R.string.service_welcome, Toast.LENGTH_SHORT)
 				.show();
 		_sensorManager.registerListener(
 				this, _acceleratorSensor, SensorManager.SENSOR_DELAY_NORMAL);
-		
+
+        _bServiceStarted = true;
 		return START_STICKY;
 	}
 	
 	@Override
 	public IBinder onBind(Intent intent) {
-		return null;
+        return _localBinder;
 	}
 
 	@Override
 	public void onDestroy() {
-		Toast.makeText(this, "Fall!", Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, R.string.service_farewell, Toast.LENGTH_SHORT).show();
+        _bServiceStarted = false;
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		float x2 = event.values[0]/SensorManager.GRAVITY_EARTH;
-		float y2 = event.values[1]/SensorManager.GRAVITY_EARTH;
-		float z2 = event.values[2]/SensorManager.GRAVITY_EARTH;
+        _fSensorX = event.values[0]/SensorManager.GRAVITY_EARTH;
+        _fSensorY = event.values[1]/SensorManager.GRAVITY_EARTH;
+        _fSensorZ = event.values[2]/SensorManager.GRAVITY_EARTH;
 		
-		double t = Math.sqrt(x2*x2 + y2*y2 + z2*z2);
+		double t = Math.sqrt(_fSensorX*_fSensorX + _fSensorY*_fSensorY + _fSensorZ*_fSensorZ);
 		
 		if (t > 2.5) {
-			stopSelf();
+            Toast.makeText(this, R.string.fall_detected, Toast.LENGTH_SHORT).show();
+            // TODO: send email, SMS and a phone ring
+            stopSelf();
 		}
 	}
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
-	
+
+    /** method for clients */
+    public float[] getData() {
+        return new float[]{_fSensorX, _fSensorY, _fSensorZ};
+    }
+
+    /** method for clients */
+    public void stop() {
+        stopSelf();
+    }
+
 	private SensorManager _sensorManager;
 	private Sensor _acceleratorSensor;
 }
--- src/com/sisemb/falldetector/MainActivity.java
+++ src/com/sisemb/falldetector/MainActivity.java
@@ -1,24 +1,176 @@
 package com.sisemb.falldetector;
 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
 import android.view.Menu;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends Activity {
+
+    private Intent _ServiceIntent;
+    private boolean _bServiceBound;
+    private FallDetector _Service;
+    private TimerHandler _TimerHandler;
+    private LocalConnection _Connection;
+
+    public class TimerHandler extends Handler {
+        private static final int DISPLAY_DATA = 1;
+        private int _nDelay;
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == DISPLAY_DATA) {
+                MainActivity.this.doGetSensorData();
+                sendEmptyMessageDelayed(DISPLAY_DATA, _nDelay);
+            }
+        }
+        public void start(int nDelay) {
+            _nDelay = nDelay;
+            sendEmptyMessageDelayed(DISPLAY_DATA, _nDelay);
+        }
+        public void stop() {
+            removeMessages(DISPLAY_DATA);
+        }
+    }
+
+    public class LocalConnection implements ServiceConnection {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            _Service = ((FallDetector.LocalBinder)service).getService();
+            MainActivity.this.onServiceConnected();
+        }
+        public void onServiceDisconnected(ComponentName className) {
+            MainActivity.this.onServiceDisconnected();
+        }
+    }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		startService(new Intent(this, FallDetector.class));
+        setContentView(R.layout.activity_main);
+
+        _ServiceIntent = new Intent(this, FallDetector.class);
+        _Connection = new LocalConnection();
+        _TimerHandler = new TimerHandler();
+        _bServiceBound = false;
+        _Service = null;
+
+        doInitialize();
 	}
 
-	@Override
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        doUnbindService();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        _TimerHandler.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+        if (checkBox.isChecked())
+            _TimerHandler.start(1000);
+    }
+
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+    private void doInitialize() {
+        Button button = (Button)findViewById(R.id.button);
+        if (FallDetector._bServiceStarted) {
+            button.setText(R.string.stop_service);
+            doBindService();
+        } else {
+            button.setText(R.string.start_service);
+        }
+    }
+
+    private void doStartService() {
+        Button button = (Button)findViewById(R.id.button);
+        button.setText(R.string.stop_service);
+        startService(_ServiceIntent);
+        doBindService();
+    }
+
+    private void doStopService() {
+        Button button = (Button)findViewById(R.id.button);
+        button.setText(R.string.start_service);
+        _Service.stop();
+    }
+
+    private void doBindService() {
+        if (!_bServiceBound)
+            bindService(_ServiceIntent, _Connection, 0);
+    }
+
+    private void doUnbindService() {
+        if (_bServiceBound) {
+            unbindService(_Connection);
+            onServiceDisconnected();
+        }
+    }
+
+    private void doGetSensorData() {
+        if (_bServiceBound) {
+            TextView textView;
+            float arData[] = _Service.getData();
+            textView = (TextView)findViewById(R.id.textView);
+            textView.setText(Float.toString(arData[0]));
+            textView = (TextView)findViewById(R.id.textView2);
+            textView.setText(Float.toString(arData[1]));
+            textView = (TextView)findViewById(R.id.textView3);
+            textView.setText(Float.toString(arData[2]));
+        }
+    }
+
+    private void onServiceConnected() {
+        _bServiceBound = true;
+        Toast.makeText(MainActivity.this, R.string.service_bound,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private void onServiceDisconnected() {
+        _Service = null;
+        _bServiceBound = false;
+        Toast.makeText(MainActivity.this, R.string.service_unbound,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton(View v) {
+        if (FallDetector._bServiceStarted)
+            doStopService();
+        else
+            doStartService();
+    }
+
+    public void onClickButton2(View v) {
+        // TODO: implement a menu to allow the user to change settings
+    }
+
+    public void onClickCheckBox(View v) {
+        RelativeLayout layout = (RelativeLayout)findViewById(R.id.sensorPane);
+        CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+        if (checkBox.isChecked()) {
+            layout.setVisibility(View.VISIBLE);
+            _TimerHandler.start(1000);
+        } else {
+            layout.setVisibility(View.INVISIBLE);
+            _TimerHandler.stop();
+        }
+    }
 }
